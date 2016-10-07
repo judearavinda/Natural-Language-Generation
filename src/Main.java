abdf3ac8230a56782426774e6309a3cf80c00c05
jsnlm <jasonjslam@gmail.com>
2016-10-06 23:36:23 -0400

    Implmented the different types of searches

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,7 @@
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Map;
-import java.util.Queue;
+import java.util.*;
 
 import classes.Graph;
 import classes.Graph.Node;
@@ -12,67 +9,91 @@ import classes.Graph.Edge;
 import classes.TraverseNode;
 
 public class Main {
+
+	enum SearchType {bfs, dfs, heurisitic};
+
 	static final String fileName = "input.txt";
 	public static void main(String[] args) {
-//		print(question1("hans", new String[]{"NNP","VBD","DT","NN"}, stringfiedFile(fileName)));
-		print(question1("benjamin" , new String[]{"NNP","VBD","DT","NN"}, stringfiedFile(fileName)));
-		print(question1("a" , new String[]{"DT","NN","VBD","NNP"}, stringfiedFile(fileName)));
-		print(question1("benjamin" , new String[]{"NNP","VBD","DT","JJS","NN"}, stringfiedFile(fileName)));
-		print(question1("a" , new String[]{"DT","NN","VBD","NNP","IN","DT","NN"}, stringfiedFile(fileName)));
+		print(sentenceGenerator("the", new String[]{"DT","NN","VBD","NNP","IN","DT","NN"}, stringfiedFile(fileName), SearchType.bfs));
+		print(sentenceGenerator("the", new String[]{"DT","NN","VBD","NNP","IN","DT","NN"}, stringfiedFile(fileName), SearchType.dfs));
+		print(sentenceGenerator("the", new String[]{"DT","NN","VBD","NNP","IN","DT","NN"}, stringfiedFile(fileName), SearchType.heurisitic));
+//		print(sentenceGenerator("benjamin" , new String[]{"NNP","VBD","DT","NN"}, stringfiedFile(fileName)));
+//		print(sentenceGenerator("a" , new String[]{"DT","NN","VBD","NNP"}, stringfiedFile(fileName)));
+//		print(sentenceGenerator("benjamin" , new String[]{"NNP","VBD","DT","JJS","NN"}, stringfiedFile(fileName)));
+//		print(sentenceGenerator("a" , new String[]{"DT","NN","VBD","NNP","IN","DT","NN"}, stringfiedFile(fileName)));
 	}
 
 
 
 
-	static String question1(String startingWord, String[] sentenceSpec, String graph){
+	static String sentenceGenerator(String startingWord, String[] sentenceSpec, String graph, SearchType searchType){
 		Graph g = textProcesser(graph);
+		if (searchType == SearchType.heurisitic){ //search for the highest probability first
+			for (ArrayList<Edge> value : g.nodes.values()) {
+				Collections.sort(value, (o1, o2) -> Double.compare(o2.probability, o1.probability ));
+			}
+		}
 		Node root = g.new Node(startingWord, sentenceSpec[0]);
 
 
-		Queue<TraverseNode> currentNodes = new ArrayDeque<>();
-		Queue<TraverseNode> childrenNodes = new ArrayDeque<>();
-		TraverseNode currentTNode = new TraverseNode(root, root.word, 1.0);
+		LinkedList<TraverseNode> currentNodes = new LinkedList<>();
+		TraverseNode currentTNode = new TraverseNode(root, root.word, 1.0, 1);
 		currentNodes.add(currentTNode);
-//		print(currentTNode);
-		for (int i = 1; i < sentenceSpec.length; i++) { //Starts at 1 because we already put in the first word that matches the spec
-			while(currentNodes.size() > 0){
-				currentTNode = currentNodes.poll();
 
-				ArrayList<Edge> childrenList = g.nodes.get(currentTNode.node);
-				if (childrenList == null){ // the node may
-					print(currentTNode.node);
+		double maxProbability = 0;
+		String maxProbabilitySentence = "something went wrong";
+		int nodesConsidered = 0;
+
+		while(currentNodes.size() > 0){
+			switch (searchType) {
+				case bfs:
+					currentTNode = currentNodes.pollFirst(); //bfs
+					break;
+				case dfs:
+					currentTNode = currentNodes.pollLast(); //dfs
+					break;
+				case heurisitic:
+					currentTNode = currentNodes.pollLast(); //dfs - heuristic uses dfs
+					break;
+			}
+			nodesConsidered++;
+			if (currentTNode.numWords == sentenceSpec.length){	//Found a final node
+				if (maxProbability < currentTNode.probability ){
+					maxProbability = currentTNode.probability;
+					maxProbabilitySentence = currentTNode.fullString;
 				}
-				for (Graph.Edge edge : childrenList) {
-					if (!edge.endNode.partofSpeech.equals(sentenceSpec[i])) {
+				continue;
+			}
+			ArrayList<Edge> childrenList = g.nodes.get(currentTNode.node);
+			for (Graph.Edge edge : childrenList) {
+				//currentTNode.numWords is actually looks at the next index because starts its count at 1, not 0
+				if (!edge.endNode.partofSpeech.equals(sentenceSpec[currentTNode.numWords])) {
 //						print(currentTNode.fullString + ' '  + edge.endNode.word);
-					}
-					else {
+				}
+				else {
+					double newNodeProbability = currentTNode.probability * edge.probability;
+					if (newNodeProbability > maxProbability){
 						TraverseNode node = new TraverseNode(
 								edge.endNode,
 								currentTNode.fullString + ' '  + edge.endNode.word,
-								currentTNode.probability * edge.probability);
-						childrenNodes.add(node);
-//						print(node);
+								newNodeProbability,
+								currentTNode.numWords+1);
+						currentNodes.add(node);
 					}
+//					print(node);
 				}
 			}
-			currentNodes = childrenNodes;
-			childrenNodes = new ArrayDeque<>();
 		}
-		double maxProbability = 0;
-		String maxProbabilitySentence = "something went wrong";
-		for (TraverseNode tn: currentNodes) {
-			if (maxProbability < tn.probability ){
-				maxProbability = tn.probability;
-				maxProbabilitySentence = tn.fullString;
-			}
-		}
-		return "“" + maxProbabilitySentence + "” with probability " + maxProbability + "\nTotal nodes considered: " + currentNodes.size();
+
+		return "“" + maxProbabilitySentence + "” with probability " + maxProbability + "\nTotal nodes considered: " + nodesConsidered;
 	}
+
+
 
 	static Graph textProcesser(String graphText) {
 		Graph graph = new Graph();
 		String[] inputLines = graphText.split("\\r\\n|\\n|\\r");
+//		Collections.shuffle(Arrays.asList(inputLines));
 		for (String line : inputLines ) {
 			// go through each line
 			line = line.replaceAll("//", "/");
--- src/classes/TraverseNode.java
+++ src/classes/TraverseNode.java
@@ -4,10 +4,12 @@ public class TraverseNode {
     public Graph.Node node;
     public String fullString;
     public double probability;
-    public TraverseNode(Graph.Node node, String  fullString, double probability){
+    public int numWords;
+    public TraverseNode(Graph.Node node, String  fullString, double probability, int numWords){
         this.node = node;
         this.fullString = fullString;
         this.probability = probability;
+        this.numWords= numWords;
     }
 
     @Override
